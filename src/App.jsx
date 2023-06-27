b02c088074fe43625e97a911aa1a42f2f30f87fc
Beth Schore <betro1@gmail.com>
2023-06-26 18:17:04 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
     return (
         <div className="App">
-            <img src={reactLogo} alt="React Logo" width="300" />
+            UCLAX-Web1-Summer2023
         </div>
     );
 }
